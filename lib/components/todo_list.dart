5957a82366137cb41270bf7ae695e107225803f7
garthendrich <garthendrich@gmail.com>
2022-11-26 14:03:59 +0800

    feat: modify to-do items design

--- lib/components/todo_list.dart
+++ lib/components/todo_list.dart
@@ -46,7 +46,7 @@ class ToDoList extends StatelessWidget {
                   ...toDos.map((toDo) {
                     return _buildToDoTile(toDo, context);
                   }).toList(),
-                  const SizedBox(height: 92)
+                  const SizedBox(height: 80)
                 ],
               );
             }),
@@ -57,16 +57,68 @@ class ToDoList extends StatelessWidget {
   }
 
   Widget _buildToDoTile(ToDo toDo, BuildContext context) {
-    return ListTile(
-      title: Text(toDo.title),
-      leading: Checkbox(
-        value: toDo.isDone,
-        onChanged: (isChecked) {},
-      ),
-      trailing: Wrap(
-        children: [
-          IconButton(icon: const Icon(Icons.more_vert), onPressed: () {})
-        ],
+    return Card(
+      color: const Color(0xFFEFF3F3),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+      elevation: 1,
+      shadowColor: Theme.of(context).colorScheme.primary,
+      child: ListTile(
+        title: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          child: Text(
+            toDo.title,
+            style: toDo.isDone
+                ? TextStyle(
+                    color: Theme.of(context).colorScheme.secondary,
+                    decoration: TextDecoration.lineThrough,
+                  )
+                : const TextStyle(),
+          ),
+        ),
+        leading: Checkbox(
+          value: toDo.isDone,
+          onChanged: (isChecked) {},
+          shape: const CircleBorder(),
+          side: BorderSide(
+            color: Theme.of(context).colorScheme.primary,
+            width: 2,
+          ),
+          fillColor: MaterialStateProperty.resolveWith((states) {
+            return Theme.of(context).colorScheme.secondary;
+          }),
+        ),
+        trailing: PopupMenuButton(
+          icon: Icon(
+            Icons.more_horiz,
+            color: toDo.isDone
+                ? Theme.of(context).colorScheme.secondary
+                : Colors.grey,
+          ),
+          splashRadius: 24,
+          itemBuilder: (BuildContext context) => [
+            PopupMenuItem(
+              child: Wrap(
+                spacing: 8,
+                crossAxisAlignment: WrapCrossAlignment.center,
+                children: [
+                  const Icon(Icons.edit, size: 20),
+                  Text("Edit", style: TextStyle(color: Colors.grey[700]))
+                ],
+              ),
+            ),
+            PopupMenuItem(
+              child: Wrap(
+                spacing: 8,
+                crossAxisAlignment: WrapCrossAlignment.center,
+                children: const [
+                  Icon(Icons.delete, size: 20, color: Colors.red),
+                  Text("Delete", style: TextStyle(color: Colors.red))
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
